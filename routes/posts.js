7892131d56717f522d36a2f2fffd7e7503807efb
Adam Hsu <projectnba77@gmail.com>
2022-06-02 17:16:07 +0800

    feat: post Route 新增 isAuth 身分驗證

--- routes/posts.js
+++ routes/posts.js
@@ -1,23 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-const handleErrorAsync = require("../service/handleErrorAsync");
-const PostController = require('../controllers/posts');
+const PostController = require('../controllers/posts')
+
+const handleErrorAsync = require("../service/handleErrorAsync")
+const { isAuth } = require('../service/isAuth')
 
 
 // 取得所有貼文
-router.get('/posts', handleErrorAsync(PostController.getPosts))
+router.get('/posts', isAuth, handleErrorAsync(PostController.getPosts))
 
 // 新增貼文
-router.post('/post', handleErrorAsync(PostController.createPost))
+router.post('/post', isAuth, handleErrorAsync(PostController.createPost))
 
 // 修改貼文
-router.patch('/post/:id', handleErrorAsync(PostController.editPost))
+router.patch('/post/:id', isAuth, handleErrorAsync(PostController.editPost))
 
 // 刪除指定貼文
-router.delete('/post/:id', handleErrorAsync(PostController.deletePost))
+router.delete('/post/:id', isAuth, handleErrorAsync(PostController.deletePost))
 
 // 刪除所有貼文
-router.delete('/posts', handleErrorAsync(PostController.deleteAllPosts))
+router.delete('/posts', isAuth, handleErrorAsync(PostController.deleteAllPosts))
 
 module.exports = router;
